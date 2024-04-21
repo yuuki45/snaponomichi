2596d14717d7111f107aab61471b6516a1796fb0
yuuki <touya_ayase@yahoo.co.jp>
2024-04-21 22:37:49 +0900

    初日

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,20 @@
 "use strict";
 
-jQuery(function ($) {
-  // この中であればWordpressでも「$」が使用可能になる
-
-  var topBtn = $('.pagetop');
-  topBtn.hide();
-
-  // ボタンの表示設定
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
-      topBtn.fadeIn();
-    } else {
-      // 画面が指定pxより上ならボタンを非表示
-      topBtn.fadeOut();
-    }
-  });
-
-  // ボタンをクリックしたらスクロールして上に戻る
-  topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
-    return false;
-  });
-
-  //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-  });
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
-  $(document).on('click', 'a[href*="#"]', function () {
-    var time = 400;
-    var header = $('header').innerHeight();
-    var target = $(this.hash);
-    if (!target.length) return;
-    var targetY = target.offset().top - header;
-    $('html,body').animate({
-      scrollTop: targetY
-    }, time, 'swing');
-    return false;
+// ハンバーガー＆ドロワーメニュー
+document.addEventListener('DOMContentLoaded', function () {
+  var headerBtn = document.querySelector('#js_header_button');
+  var drawer = document.querySelector('.drawer');
+  var drawerBg = document.querySelector('.drawer_bg');
+  var drawerLinks = document.querySelectorAll('.drawer a');
+  function toggleDrawer() {
+    headerBtn.classList.toggle('active');
+    drawer.classList.toggle('active');
+    drawerBg.classList.toggle('active');
+  }
+  headerBtn.addEventListener('click', toggleDrawer);
+  drawerLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      toggleDrawer();
+    });
   });
 });
