543f3d118243e097b40111ca218259b9a0875800
yuuki <touya_ayase@yahoo.co.jp>
2024-04-21 22:44:31 +0900

    初日

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,21 @@
+// ハンバーガー＆ドロワーメニュー
+document.addEventListener('DOMContentLoaded', function() {
+  const headerBtn = document.querySelector('#js_header_button');
+  const drawer = document.querySelector('.drawer');
+  const drawerBg = document.querySelector('.drawer_bg');
+  const drawerLinks = document.querySelectorAll('.drawer a');
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  function toggleDrawer() {
+    headerBtn.classList.toggle('active');
+    drawer.classList.toggle('active');
+    drawerBg.classList.toggle('active');
+  }
 
-  var topBtn = $('.pagetop');
-  topBtn.hide();
+  headerBtn.addEventListener('click', toggleDrawer);
 
-  // ボタンの表示設定
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
-      topBtn.fadeIn();
-    } else {
-      // 画面が指定pxより上ならボタンを非表示
-      topBtn.fadeOut();
-    }
+  drawerLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+      toggleDrawer();
+    });
   });
-
-  // ボタンをクリックしたらスクロールして上に戻る
-  topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
-    return false;
-  });
-
-  //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-
-  });
-
-
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
-  $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
-  });
-
 });
